b5b5f3e6e5a25322a1cc5251d4ed896f6a8997c7
Aldo Urtusuastegui <aldithomagno@gmail.com>
2021-01-15 18:33:24 -0700

    se elimino fab

--- app/src/main/java/com/acsoft/savepassword/MainActivity.kt
+++ app/src/main/java/com/acsoft/savepassword/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.acsoft.savepassword
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
@@ -18,11 +16,5 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 }
