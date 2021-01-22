1564231690e9f51ae8067c2ee7b28bd381057fd8
Aldo Urtusuastegui <aldithomagno@gmail.com>
2021-01-21 21:43:16 -0700

    metodo para establecer colores de circulos

--- app/src/main/java/com/acsoft/savepassword/ui/adapters/AccountAdapter.kt
+++ app/src/main/java/com/acsoft/savepassword/ui/adapters/AccountAdapter.kt
@@ -1,9 +1,13 @@
 package com.acsoft.savepassword.ui.adapters
 
 import android.content.Context
+import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.acsoft.savepassword.R
 import com.acsoft.savepassword.core.BaseViewHolder
 import com.acsoft.savepassword.data.model.Account
 import com.acsoft.savepassword.databinding.AccountItemBinding
@@ -25,7 +29,9 @@ class AccountAdapter: RecyclerView.Adapter<BaseViewHolder<*>>() {
 
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
         when(holder) {
-            is AccountViewHolder -> holder.bind(accountList[position])
+            is AccountViewHolder -> {
+                holder.bind(accountList[position])
+            }
         }
     }
 
@@ -39,6 +45,44 @@ class AccountAdapter: RecyclerView.Adapter<BaseViewHolder<*>>() {
             binding.tvTitle.text = item.title
             binding.tvUsername.text = item.name
             binding.tvDate.text = item.date
+            binding.tvMark.text = item.title.first().toString().capitalize()
+            Log.d("NEW",adapterPosition.toString())
+            circleColor(binding,context,adapterPosition)
+        }
+    }
+
+    private fun circleColor(binding : AccountItemBinding,context: Context,position: Int) {
+
+        var color :Int = position
+        if (position>=5) {
+            color = 0
+        }
+
+        when (color) {
+            0 -> {
+                binding.tvMark.backgroundTintList = ContextCompat.getColorStateList(context, R.color.color_one)
+                color.inc()
+            }
+
+            1 -> {
+                binding.tvMark.backgroundTintList = ContextCompat.getColorStateList(context, R.color.color_two)
+                color.inc()
+            }
+
+            2 -> {
+                binding.tvMark.backgroundTintList = ContextCompat.getColorStateList(context, R.color.color_three)
+                color.inc()
+            }
+
+            3 -> {
+                binding.tvMark.backgroundTintList = ContextCompat.getColorStateList(context, R.color.color_four)
+                color.inc()
+            }
+
+            4 -> {
+                binding.tvMark.backgroundTintList = ContextCompat.getColorStateList(context, R.color.color_five)
+                color.inc()
+            }
         }
 
     }
