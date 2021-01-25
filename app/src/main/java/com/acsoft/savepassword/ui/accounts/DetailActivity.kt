b50cd7d219bae3005213293aa9f16e8c3d6f52eb
Aldo Urtusuastegui <aldithomagno@gmail.com>
2021-01-24 17:31:21 -0700

    se agrego menu toolbar

--- app/src/main/java/com/acsoft/savepassword/ui/accounts/DetailActivity.kt
+++ app/src/main/java/com/acsoft/savepassword/ui/accounts/DetailActivity.kt
@@ -2,6 +2,10 @@ package com.acsoft.savepassword.ui.accounts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import com.acsoft.savepassword.R
 import com.acsoft.savepassword.databinding.ActivityDetailBinding
 import com.acsoft.savepassword.databinding.FragmentFavoritesBinding
@@ -9,6 +13,8 @@ import com.acsoft.savepassword.databinding.FragmentFavoritesBinding
 class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
+
+    private var isFavorite: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +26,27 @@ class DetailActivity : AppCompatActivity() {
 
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.item_favorite -> {
+                isFavorite = if (isFavorite) {
+                    item.setIcon(R.drawable.ic_favorite_disabled)
+                    false
+                } else {
+                    item.setIcon(R.drawable.ic_favorite_enabled)
+                    true
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
