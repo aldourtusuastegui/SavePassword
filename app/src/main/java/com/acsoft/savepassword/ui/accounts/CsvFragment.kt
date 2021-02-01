a08794ef6cdb00082c3417dafa86f8b8c0760adc
Aldo Urtusuastegui <aldithomagno@gmail.com>
2021-02-01 16:28:39 -0700

    Se genera csv

--- app/src/main/java/com/acsoft/savepassword/ui/accounts/CsvFragment.kt
+++ app/src/main/java/com/acsoft/savepassword/ui/accounts/CsvFragment.kt
@@ -1,19 +1,38 @@
 package com.acsoft.savepassword.ui.accounts
 
+import android.accounts.Account
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.acsoft.savepassword.R
+import com.acsoft.savepassword.data.local.AppDatabase
+import com.acsoft.savepassword.data.local.LocalAccountDataSource
 import com.acsoft.savepassword.databinding.FragmentCsvBinding
+import com.acsoft.savepassword.presentation.AccountViewModel
+import com.acsoft.savepassword.presentation.AccountViewModelFactory
+import com.acsoft.savepassword.repository.AccountRepositoryImpl
+import com.acsoft.utils.generateFile
+import com.acsoft.utils.goToFileIntent
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
+import java.io.File
+
 
 class CsvFragment : Fragment() {
 
     private lateinit var binding: FragmentCsvBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    private val viewModel by viewModels<AccountViewModel> {
+        AccountViewModelFactory(AccountRepositoryImpl(LocalAccountDataSource(AppDatabase.getDatabase(requireContext()).AccountDao())))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_csv, container, false)
     }
@@ -24,13 +43,39 @@ class CsvFragment : Fragment() {
 
 
         binding.btnExportCsv.setOnClickListener {
-
+            exportDatabaseToCSVFile()
         }
     }
 
 
-    private fun exportCsv() {
+    companion object {
+        private var MOVIES_SHOWN = true
+    }
 
+    private fun getCSVFileName() : String =
+        if (MOVIES_SHOWN) "MoviesRoomExample.csv" else "DirectorsRoomExample.csv"
+
+    private fun exportDatabaseToCSVFile() {
+        val csvFile = generateFile(requireContext(), getCSVFileName())
+        if (csvFile != null) {
+            exportMoviesWithDirectorsToCSVFile(csvFile)
+            Toast.makeText(requireContext(), "Generado", Toast.LENGTH_LONG).show()
+            val intent = goToFileIntent(requireContext(), csvFile)
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(),"No generado", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun exportMoviesWithDirectorsToCSVFile(csvFile: File) {
+        csvWriter().open(csvFile, append = false) {
+            // Header
+            writeRow(listOf("[id]", "[jola]", "[ejemplo]"))
+            writeRow(listOf("[id]", "[jola]", "[ejemplo]"))
+            writeRow(listOf("[id]", "[jola]", "[ejemplo]"))
+            writeRow(listOf("[id]", "[jola]", "[ejemplo]"))
+        }
     }
 
 }
--- app/src/main/java/com/acsoft/utils/Utils.kt
+++ app/src/main/java/com/acsoft/utils/Utils.kt
@@ -3,11 +3,14 @@ package com.acsoft.utils
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.core.content.FileProvider
 import com.acsoft.savepassword.R
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.snackbar.Snackbar.make
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -34,4 +37,26 @@ fun copyToClipboard(view: View,context: Context,text: String)  {
     val clipData = ClipData.newPlainText("text", text)
     clipboardManager.setPrimaryClip(clipData)
     make(view,context.getString(R.string.copied_clipboard),Snackbar.LENGTH_LONG).show()
+}
+
+
+fun generateFile(context: Context, fileName: String): File? {
+    val csvFile = File(context.filesDir, fileName)
+    csvFile.createNewFile()
+
+    return if (csvFile.exists()) {
+        csvFile
+    } else {
+        null
+    }
+}
+
+fun goToFileIntent(context: Context, file: File): Intent {
+    val intent = Intent(Intent.ACTION_VIEW)
+    val contentUri = FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", file)
+    val mimeType = context.contentResolver.getType(contentUri)
+    intent.setDataAndType(contentUri, mimeType)
+    intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+
+    return intent
 }
